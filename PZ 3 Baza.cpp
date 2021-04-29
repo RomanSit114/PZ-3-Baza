f2412b8bad7b1c535558087d74ad37dbb3190645
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2021-04-30 01:10:42 +0300

    Add files via upload

---
+++ PZ 3 Baza.cpp
@@ -0,0 +1,33 @@
+﻿// PZ 3 Baza.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter values" << endl;
+    int mas[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> mas[i];
+        //cout << mas[i] << " ";
+    }
+    cout << "massive before sorting" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << mas[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10 - 1 - i; j++) {
+            if (mas[j] > mas[j + 1]) {
+                swap(mas[j], mas[j + 1]);
+            }
+        }
+    }
+    cout << "massive after sorting" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << mas[i] << " ";
+    }
+}
+
+
